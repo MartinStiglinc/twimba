d9229bf89416a96c609a7d48aef001c4d7344852
Martin Štiglinc <martin.stiglinc@gmail.com>
2023-04-02 20:46:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { tweetsData } from './data.js'
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
+let tweetsData = tweetsData
 let tweetsFromLocalStorage = JSON.parse( localStorage.getItem("tweetsData") )
 
 if (tweetsFromLocalStorage) {
